a16f3fd1a20858ac42fcb7569b39c126d2983e40
JLSC24 <jlsc24dg@gmail.com>
2021-08-08 22:54:09 -0500

    Configuración para amplify

--- src/app/modules/core/services/cognito/cognito.service.ts
+++ src/app/modules/core/services/cognito/cognito.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
 import Amplify, { Auth } from 'aws-amplify';
 
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-2:342db6ba-3d18-4dff-8ffa-2315ff0cf318',
+    region: 'us-east-2',
+    userPoolId: 'us-east-2_M39Idxzru',
+    userPoolWebClientId: '49qntc23sv31ckdunjpujhplue',
+  },
+});
+Auth.configure({
+  Auth: {
+    identityPoolId: 'us-east-2:342db6ba-3d18-4dff-8ffa-2315ff0cf318',
+    region: 'us-east-2',
+    userPoolId: 'us-east-2_M39Idxzru',
+    userPoolWebClientId: '49qntc23sv31ckdunjpujhplue',
+  },
+});
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,6 +25,7 @@ export class CognitoService {
   constructor() {}
 
   async getAuthenticatedUser(): Promise<boolean> {
+    console.log(Auth.currentAuthenticatedUser());
     try {
       await Auth.currentAuthenticatedUser();
       console.log('Talogeao');
