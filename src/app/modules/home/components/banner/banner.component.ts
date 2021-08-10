e569d797a78460d076fde273693407cbe1237ad3
JLSC24 <jlsc24dg@gmail.com>
2021-08-10 14:10:59 -0500

    Finalización sprint 1

--- src/app/modules/home/components/banner/banner.component.ts
+++ src/app/modules/home/components/banner/banner.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent {
   images: string[] = [
+    'assets/images/banner-3.jpg',
+    'assets/images/banner-4.jpg',
     'assets/images/banner-1.jpg',
     'assets/images/banner-2.jpg',
-    'assets/images/banner-3.jpg',
   ];
 
   constructor() {}
